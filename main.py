788d8ff84e2cafdb8d7e64267cecc048f5421006
shkadov <PerfaV0RE>
2018-05-03 20:08:42 +0300

    fixed some strings

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-#from config_file import file_ini, file_ini_check
-#from check_path import check_path, log_path
-#from remove_file import remove_file
 from config_file import FileINICheck, file_ini
 from check_path import CheckPath, log_path
 from remove_file import FileRemove, log_path
