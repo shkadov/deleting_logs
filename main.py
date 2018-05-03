56ef8ffca92c6c2c77991c37ce448054f90c782c
shkadov <PerfaV0RE>
2018-05-03 17:54:42 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import os, time, logging
+
+from datetime import datetime
+
+
+logs_path = ''
+logfile   = os.getcwd() + '/retention_log.txt'
+retention = 32
+current_time = time.time()
+current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+def remove_file(logs_path):
+    try:
+        for dir, subdir, files in os.walk(logs_path):
+            for file in files:
+                file_name = os.path.join(dir, file)
+                file_date = os.path.getmtime(file_name)
+                file_age = (int(current_time) - int(file_date)) / 86400  # get current file age
+                if file_age > retention:
+                    with open(logfile, 'a') as f:
+                        f.write(str(current_date) + ' File ' + str(file_name) + ' has been deleted (modify date: ' + str(datetime.fromtimestamp(file_date)) +' )\n')
+                        f.close()
+                    #os.remove(file_name)
+
+    except BaseException as e:
+        logging.error(str(e))
+
+remove_file(logs_path)
